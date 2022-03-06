722f5ea472190065b3d1beab6207b9af0706d49d
Mehmet Ulrich <m.ulrich@uni-wuppertal.de>
2022-03-06 10:16:17 +0100

    test

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -11,7 +11,7 @@ const int motor_pin_2 = 9;
 const int door_signal = A0;
 const int endswitch_1 = A1;
 const int endswitch_2 = A2;
-const int current_measure = A3;
+const int current_sensor = A3;
 
 int button_state_signal;
 int last_button_state_signal;
@@ -22,9 +22,11 @@ unsigned long last_debounce_time;
 unsigned long debounce_delay = 50;
 
 //Function Prototypes
+uint8_t open_door();
 void force_open_door();
 void force_close_door();
 void stop_door();
+int current_value();
 
 void setup()
 {
@@ -38,7 +40,7 @@ void setup()
 	pinMode(door_signal, INPUT);
 	pinMode(endswitch_1, INPUT);
 	pinMode(endswitch_2, INPUT);
-	pinMode(current_measure, INPUT);
+	pinMode(current_sensor, INPUT);
 }
 
 void loop()
@@ -64,25 +66,48 @@ void loop()
 		}
 	}
 	last_button_state_signal = reading_signal_button;
+	Serial.println(current_value());
+	stop_door();
 }
 
-void force_open_door()
+uint8_t open_door()
 {
-	Serial.println("opening door");
 	analogWrite(motor_pin_1, 255);
 	analogWrite(motor_pin_2, 0);
+	if ((digitalRead(endswitch_1) == LOW) || (digitalRead(endswitch_2) == LOW)) {
+		stop_door();
+	}
+	if (digitalRead(door_signal == HIGH)) {
+		stop_door();
+	}
+
+}
+void force_open_door()
+{
+	//Serial.println("opening door");
+	analogWrite(motor_pin_1, 255);
+	analogWrite(motor_pin_2, 0);
+	Serial.println(current_value());
 }
 
 void force_close_door()
 {
-	Serial.println("closing door");
+	//Serial.println("closing door");
 	analogWrite(motor_pin_1, 0);
 	analogWrite(motor_pin_2, 255);
+	Serial.println(current_value());
 }
 
 void stop_door()
 {
-	Serial.println("stopping door");
 	analogWrite(motor_pin_1, 0);
 	analogWrite(motor_pin_2, 0);
+}
+
+int current_value()
+{
+	int value;
+	value = analogRead(current_sensor);
+	//value = map(value, 0, 1023, -512, 511);
+	return value;
 }
