9797e3d1327d192578124d8669017f745c3afb15
Mehmet Ulrich <m.ulrich@uni-wuppertal.de>
2023-08-12 15:29:46 +0200

    stuff

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -245,7 +245,7 @@ int current_value()
 		value_sum += value;
 	}
 	value_sum = value_sum/5;
-	if ((value_sum < 20) && (value_sum > -20))  return 0;
+	if ((value_sum < 50) && (value_sum > -50))  return 0;
 	if ((value_sum > 250) || (value_sum < -250)) return 512;
 	return value_sum;
 }
